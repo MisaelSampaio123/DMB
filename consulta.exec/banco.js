3ba43e53d700bae48f39b7c0ba721c0e6379aa51
MisaelSampaio123 <misaelsampaio1905@gmail.com>
2026-04-03 17:02:30 -0400

    primeiro commit

---
+++ consulta.exec/banco.js
@@ -0,0 +1,27 @@
+// banco.js - Dados Padronizados (Apenas números no CPF)
+const suspeitos = [
+    {
+        nome: "Ricardo Alcantara Cavalcanti",
+        cpf: "12345612345", // Removidos os pontos e traço
+        foto: "fotos/registro1.jpg",
+        armas: "Pistola Taurus G3C",
+        calibre: "9mm",
+        serie: "ABC123456"
+    },
+    {
+        nome: "Beatriz Helena Soares",
+        cpf: "22222222222",
+        foto: "fotos/registro2.jpg",
+        armas: "Fuzil Imbel IA2",
+        calibre: "5.56 NATO",
+        serie: "XYZ987654"
+    },
+    {
+        nome: "Misael Araújo Sampaio",
+        cpf: "03348451221",
+        foto: "fotos/fotominha.jpeg",
+        armas: "Rifle CBC 7022",
+        calibre: ".22 LR",
+        serie: "DEF456789"
+    }
+];
